3e9f2b3a86241b113dcfca992eb6cba3906d69be
upiw256 <bilqimlb@gmail.com>
2026-02-02 23:34:37 +0700

    menambahkan versi

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -84,16 +84,14 @@ export default function LoginPage() {
           </div>
 
           {/* 2. Status Database (Baru) */}
-          {dbName && (
             <div className="flex items-center gap-2 bg-blue-50 px-3 py-1.5 rounded-full border border-blue-100 shadow-sm">
               <svg className="w-3 h-3 text-blue-600" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 7v10c0 2.21 3.582 4 8 4s8-1.79 8-4V7M4 7c0 2.21 3.582 4 8 4s8-1.79 8-4M4 7c0-2.21 3.582-4 8-4s8 1.79 8 4m0 5c0 2.21-3.582 4-8 4s-8-1.79-8-4" />
               </svg>
-              {/* <span className="text-xs font-bold text-blue-700">
-                DB: {dbName}
-              </span> */}
+              <span className="text-xs font-bold text-blue-700">
+                Versi: {process.env.NEXT_PUBLIC_VERSION}
+              </span>
             </div>
-          )}
 
         </div>
         {/* ================================== */}
